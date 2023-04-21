1865cb0fb27ce03d31379453fbfb616b15331726
Skeyron <wassim.chenouf@univ-tlse3.fr>
2023-04-21 14:56:57 +0200

    EXG_1

---
+++ src/ilu2/Welcome.java
@@ -0,0 +1,18 @@
+package ilu2;
+
+public class Welcome {
+	
+	
+	
+	
+	
+	public static String welcome(String input) {
+		StringBuilder reponse = new StringBuilder();
+		
+		reponse.append("Hello, " + input);
+		
+		return reponse.toString();
+	}
+	
+	
+}
